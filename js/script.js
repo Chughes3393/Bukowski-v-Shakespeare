7ead6cba22a469662906ed740362bf1cfefade95
chughes3393 <hughes.c33@gmail.com>
2021-12-17 14:30:56 -0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* ========== Classes ========== */
+
+class BestPoet {
+    constructor(obj, arr)
+} 
+
+class Bukowski extends BestPoet {
+
+}
+
+class Shakespeare extends BestPoet {
+
+}
+
+// what goes inside classes? Is it best to use classes here? 
+
+/* ========== Objects ========== */
+
+const hank = [];
+const billy = [];
+// need to make both of these const new versions of class Bukowski & Shakespeare respectively
+// need to have them iterate over arrays 
+
+/* ========== DOM // Query Selectors ========== */
+
+
+/* ========== Functions ========== */
+
+function CommenceDuel() {
+// this is where the game happens 
+}
+
+// function is called
+CommenceDuel(Hank, Billy)
